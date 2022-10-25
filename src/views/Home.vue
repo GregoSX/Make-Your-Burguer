84b2190fd80e470c5b47fd05f662e3fbcd67425d
Guilherme Grego <guilhermegrego376@gmail.com>
2022-10-25 12:01:35 -0300

    feat: Banner

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,18 @@
 <template>
+  <BannerVue />
   <div class="main-container">
-      <h1>HOME</h1>
+      <h1>Monte seu burguer</h1>
+      <p>formulario</p>
   </div>
 </template>
 
 <script>
+  import BannerVue from '../components/Banner.vue';
 
+  export default {
+    name: 'Home',
+    components: {
+      BannerVue
+    }
+  }
 </script>
