98d22c5e1e76fbd6bb18b6e8418690aed084c9c4
Guilherme Grego <guilhermegrego376@gmail.com>
2022-10-25 18:59:14 -0300

    feat: Resgatando dados do bd

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -10,24 +10,24 @@
             <div class="input-container">
                 <label for="bread">Choose the bread</label>
                 <select name="bread" id="bread" v-model="bread">
-                    <option value=""> Select your bread </option>
-                    <option value="brown">Brown bread</option>
+                    <option disabled value="" > Select your bread </option>
+                    <option v-for="bread in breads" :key="bread.id" :value="bread.tipo"> {{bread.tipo}} </option>
                 </select>
             </div>
 
             <div class="input-container">
                 <label for="meat">Choose your burguer meat</label>
                 <select name="meat" id="meat" v-model="meat">
-                    <option value=""> Select your meat </option>
-                    <option value="steak">Rump steak</option>
+                    <option disabled value=""> Select your meat </option>
+                    <option v-for="meat in meatData" :key="meat.id" :value="meat.tipo"> {{meat.tipo}} </option>
                 </select>
             </div>
 
             <div id="options-container" class="input-container">
                 <label id="options-title" for="options">Select the options</label>
-                <div id="checkbox-container">
-                    <input type="checkbox" name="options" id="options" v-model="options" value="salami">
-                    <span>Salami</span>
+                <div id="checkbox-container" v-for="option in optionsData" :key="option.id" >
+                    <input type="checkbox" name="options" id="options" v-model="options" :value="option.tipo">
+                    <span>{{option.tipo}}</span>
                 </div>
             </div>
 
@@ -41,7 +41,34 @@
 <script>
     export default {
         name: 'Form',
+        data() {
+            return {
+                breads: null,
+                meatData: null,
+                optionsData: null,
+                name: null,
+                bread: null,
+                meat: null,
+                options: [],
+                status: 'Requested',
+                msg: null
+            }
+        },
+        methods: {
+            async getIngredients() {
+                const req = await fetch("http://localhost:3000/ingredientes")
 
+                const data = await req.json()
+
+                this.breads = data.paes
+                this.meatData = data.carnes
+                this.optionsData = data.opcionais
+
+            }
+        },
+        mounted() {
+            this.getIngredients()
+        }
     }
 </script>
 
